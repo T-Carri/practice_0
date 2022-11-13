091c642bcb98a85585f57b6c04a5151cdad66501
unknown <bryan_narutito@hotmail.com>
2022-11-12 18:29:25 -0600

    12112022

--- src/pages/perfil/Perfil.jsx
+++ src/pages/perfil/Perfil.jsx
@@ -1,8 +1,10 @@
 import React, {useEffect, useContext} from 'react'
 import { useNavigate } from 'react-router-dom'
-import {Button, NavDropdown, Nav} from 'react-bootstrap'
+import {Button, NavDropdown, Nav, Navbar, Container} from 'react-bootstrap'
 import UserContext from '../../context/UserContext'
 import './Perfil.css'
+import logo from './logo.svg'
+import  castle from './castle.png'
 export const Perfil = () => {
   const handleSelect = (eventKey) => alert(`selected ${eventKey}`);
   const {logout}=useContext(UserContext)
@@ -35,35 +37,27 @@ navigate('/home')
   return (
     <body>
 <header>
+<Navbar  className="NavBar"  >
+        <Container >
+          <Navbar.Brand href="#home">
+            <img 
+        
+              src={castle}
+              width="59"
+              height="52"
+              className="castle"
+              alt="Tarup"
+              
+            />
+          </Navbar.Brand>
+        </Container>
+        
+      </Navbar>
+     
 
-<Nav className="NavBar" variant="pills" activeKey="1" onSelect={handleSelect}>
-    <Nav.Item>
-      <Nav.Link eventKey="1" href="#/home">
-        NavLink 1 content
-      </Nav.Link>
-    </Nav.Item>
-    <Nav.Item>
-      <Nav.Link eventKey="2" title="Item">
-        NavLink 2 content
-      </Nav.Link>
-    </Nav.Item>
-    <Nav.Item>
-      <Nav.Link eventKey="3" disabled>
-        NavLink 3 content
-      </Nav.Link>
-    </Nav.Item>
-    <NavDropdown title="Funciones" id="nav-dropdown">
-      <NavDropdown.Item eventKey="4.1">Action</NavDropdown.Item>
-      <NavDropdown.Item eventKey="4.2">Another action</NavDropdown.Item>
-      <NavDropdown.Item eventKey="4.3">Something else here</NavDropdown.Item>
-      <NavDropdown.Divider />
-      <NavDropdown.Item eventKey="4.4">Separated link</NavDropdown.Item>
-    </NavDropdown>
-    <Nav.Item>   <Button onClick={logoutHandle}> Salir</Button> </Nav.Item>
-  </Nav>
       
   </header>
-  <section className='root'>
+  
 
   <section   className='section1'>
   <h1>Seccion 1</h1>
@@ -74,10 +68,10 @@ tu objetivo es: lograr que una ves logueado mantenga el log de manera profesiona
 </section>
 <section   className='section2'>
   <div className='tarup'><h1>Div 2</h1> </div>
-  <div className='extras'><h1>Div 3</h1> </div>
+  
 </section>
 
-  </section>
+  
 
 
     </body>
